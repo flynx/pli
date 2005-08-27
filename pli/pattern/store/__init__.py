80bd27ee69bde64a804cff48da7b901bd75449c8
alex_nanou <alex_nanou>
2005-08-27 12:56:38 +0000

    *** empty log message ***

--- pli/pattern/store/__init__.py
+++ pli/pattern/store/__init__.py
@@ -1,7 +1,7 @@
 #=======================================================================
 
 __version__ = '''0.0.01'''
-__sub_version__ = '''20040213021557'''
+__sub_version__ = '''20050827165352'''
 __copyright__ = '''(c) Alex A. Naanou 2003'''
 
 
@@ -11,7 +11,11 @@ from store import *
 from stored import *
 from storeclient import *
 
-
+# ...some peaple got REALY disappointed when reading this source tree
+# so here is a litle fix: "FUCK"
+#
+# NOTE: this is the only exception to the "no F* words policy" in pli :)
+#
 
 #=======================================================================
 #                                            vim:set ts=4 sw=4 nowrap :
